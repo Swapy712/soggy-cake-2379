35a414d8760a693df1b491a9ca8242f77bfdfb09
Swapnil yadav <yadavswapnil712@gmail.com>
2022-07-24 11:53:08 +0530

    day-3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/navbar';
+import BodyTop from './components/body.jsx/bodytop';
 
 function App() {
   return (
     <div className="App">
         <Navbar/> 
+        <BodyTop/>
     </div>
   );
 }
---
+++ src/components/body.jsx/bodytop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function BodyTop() {
+  return (
+    <div style={{
+        width:"100%",
+        height:"200px"
+    }}>
+        <img SRC="https://cdn.shopify.com/s/files/1/0449/5225/6667/files/fruit_box.png?v=1600179109"/>   
+    </div>
+  )
+}
+
+export default BodyTop
